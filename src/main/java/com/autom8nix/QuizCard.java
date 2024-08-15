60a1ad076ee01dc571d4785089d1ac8c6e013b86
Steve Casazza <sjc1014@gmail.com>
2024-08-14 21:25:19 -0400

    Created New Class QuizCard

---
+++ src/main/java/com/autom8nix/QuizCard.java
@@ -0,0 +1,28 @@
+package com.autom8nix;
+
+public class QuizCard {
+
+    private String question;
+    private String answer;
+
+    public QuizCard(String question, String answer) {
+        this.question = question;
+        this.answer = answer;
+    }
+
+    public String getAnswer() {
+        return answer;
+    }
+
+    public void setAnswer(String answer) {
+        this.answer = answer;
+    }
+
+    public String getQuestion() {
+        return question;
+    }
+
+    public void setQuestion(String question) {
+        this.question = question;
+    }
+}
