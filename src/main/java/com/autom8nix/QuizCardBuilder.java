aadd52d5385787274b31b14a6221efb8f3a95710
Steve Casazza <sjc1014@gmail.com>
2024-08-14 22:13:22 -0400

    Created JFrame and JPanel mainPanel.

--- src/main/java/com/autom8nix/QuizCardBuilder.java
+++ src/main/java/com/autom8nix/QuizCardBuilder.java
@@ -1,6 +1,7 @@
 package com.autom8nix;
 
 import javax.swing.*;
+import java.awt.*;
 import java.util.ArrayList;
 
 public class QuizCardBuilder {
@@ -8,4 +9,24 @@ public class QuizCardBuilder {
     private JTextArea answer;
     private ArrayList<QuizCard> cardList;
     private JFrame frame;
+
+    public static void main(String[] args) {
+        QuizCardBuilder builder = new QuizCardBuilder();
+        builder.go();
+    }
+
+    private void go() {
+        frame = new JFrame("Quiz Card Builder");
+        JPanel mainPanel = new JPanel();
+        Font bigFont = new Font("sanserif", Font.BOLD, 24);
+        question = new JTextArea(6, 20);
+        question.setLineWrap(true);
+        question.setWrapStyleWord(true);
+        question.setFont(bigFont);
+
+
+        frame.getContentPane().add(BorderLayout.CENTER, mainPanel);
+        frame.setSize(500,600);
+        frame.setVisible(true);
+    }
 }
