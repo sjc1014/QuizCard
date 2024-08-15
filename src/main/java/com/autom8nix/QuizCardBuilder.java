a7f85730031a636fb407ca47ab4a298a4b8898c3
Steve Casazza <sjc1014@gmail.com>
2024-08-14 22:04:33 -0400

    Created New Class QuizCardBuilder

---
+++ src/main/java/com/autom8nix/QuizCardBuilder.java
@@ -0,0 +1,11 @@
+package com.autom8nix;
+
+import javax.swing.*;
+import java.util.ArrayList;
+
+public class QuizCardBuilder {
+    private JTextArea question;
+    private JTextArea answer;
+    private ArrayList<QuizCard> cardList;
+    private JFrame frame;
+}
